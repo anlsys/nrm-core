9755e2cefbe3547557c738e35bddf2d41da87d50
Valentin Reis <vreis@anl.gov>
2019-09-16 14:34:28 -0500

    "unbreak" json-schema

--- dev/pkgs/hsnrm/default.nix
+++ dev/pkgs/hsnrm/default.nix
@@ -4,7 +4,9 @@ let
     overrides = self: super:
       with pkgs.haskell.lib; rec {
         regex = pkgs.haskell.lib.doJailbreak super.regex;
-        json-schema = pkgs.haskell.lib.doJailbreak super.json-schema;
+        json-schema =
+          (pkgs.haskell.lib.doJailbreak super.json-schema).overrideAttrs
+          (attrs: { meta = attrs.meta // { broken = false; }; });
         zeromq4-conduit = pkgs.haskell.lib.dontCheck super.zeromq4-conduit;
         hsnrm = (self.callCabal2nix "hsnrm" (hslib.filter src)) { };
         dhall = super.dhall_1_24_0;
