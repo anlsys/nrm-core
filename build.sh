7f3c0ca4b788a7575b2bc57224fc7a131c8e5333
Valentin Reis <vreis@anl.gov>
2020-01-30 13:44:50 -0600

    Change binary cache URL.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nix-build -A nrm --option extra-substituters http://129.114.24.212 --option trusted-public-keys example-nix-cache-1:HSwzbJmGDidTrax3Lvx1vMSvto04VN2O5cjfXAG9uz0=
+nix-build -A nrm --option extra-substituters http://129.114.24.212/store --option trusted-public-keys example-nix-cache-1:HSwzbJmGDidTrax3Lvx1vMSvto04VN2O5cjfXAG9uz0=
--- shake.sh
+++ shake.sh
@@ -6,7 +6,7 @@ if [ -z "$IN_NIX_SHELL" ]
 then
   nix-shell \
     --run "runhaskell dev/shake.hs $*" \
-    --option extra-substituters http://129.114.24.212 \
+    --option extra-substituters http://129.114.24.212/store \
     --option trusted-public-keys example-nix-cache-1:HSwzbJmGDidTrax3Lvx1vMSvto04VN2O5cjfXAG9uz0=
 else
   runhaskell dev/shake.hs $*
