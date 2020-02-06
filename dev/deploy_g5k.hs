5fa92b2f563ac1b06b8179792a63c95a27c40407
Valentin Reis <vreis@anl.gov>
2020-02-05 23:38:08 -0600

    format

--- dev/deploy_g5k.hs
+++ dev/deploy_g5k.hs
@@ -26,7 +26,6 @@ import qualified System.IO as SIO
   )
 
 ------ COMMON
-
 newtype UID = UID Int
   deriving (Generic)
   deriving (Show, FromJSON, ToJSON) via Int
@@ -60,7 +59,6 @@ newtype User = User Text
 newtype Password = Password Text
 
 ------ CLI
-
 newtype NodeCount = NodeCount Int
   deriving (Show, Generic)
   deriving (ToJSON) via Int
@@ -94,7 +92,6 @@ data AllCLI
       }
 
 ------ REQUESTS
-
 data ReservationRequest
   = ReservationRequest
       { resources :: Text,
@@ -128,7 +125,9 @@ hosts :: CommonOpts -> UID -> IO (Maybe Hosts)
 hosts cOpts@(CommonOpts _ _ (Site s)) jobID = do
   r <-
     getWith (httpOpts cOpts) $
-      "https://api.grid5000.fr/3.0/sites/" <> toS s <> "/jobs/"
+      "https://api.grid5000.fr/3.0/sites/"
+        <> toS s
+        <> "/jobs/"
         <> show jobID
         <> "?pretty"
   return $ (r ^? responseBody . A.key "assigned_nodes") >>= parseMaybe parseJSON
@@ -151,7 +150,7 @@ deploy cOpts@(CommonOpts _ _ (Site s)) (DeployCLI hosts (Pubfile pubkey) environ
   let req = DeployRequest {nodes = hosts, environment = environment, key = key}
   putText $ toS (encode req)
   r <- postWith (httpOpts cOpts) ("https://api.grid5000.fr/3.0/sites/" <> toS s <> "/deployments") (toJSON req)
-  return $ r ^. responseStatus . statusCode == 200
+  if r ^. responseStatus . statusCode == 201 then return True else print r >> return False
 
 allOperations :: CommonOpts -> AllCLI -> IO ()
 allOperations c r = do
@@ -165,33 +164,38 @@ allOperations c r = do
     tryHosts jobid =
       (prompt "Try to get hosts? [y/Y]" <&> (`elem` ["y", "Y"])) >>= \case
         False -> exitSuccess
-        True -> hosts c jobid >>= \case
-          Nothing -> putText "no hosts associated yet" >> tryHosts jobid
-          Just hs -> tryDeploy hs
+        True ->
+          hosts c jobid >>= \case
+            Nothing -> putText "no hosts associated yet" >> tryHosts jobid
+            Just hs -> tryDeploy hs
     tryDeploy hs =
       (prompt "Try to deploy? [y/Y]" <&> (`elem` ["y", "Y"])) >>= \case
         False -> exitSuccess
-        True -> deploy c (DeployCLI {deployHosts = hs, pubfile = allPubfile r, envir = allEnvironment r}) >>= \case
-          False -> putText "deployment couldn't succeed.. " >> tryDeploy hs
-          True -> putText "deployment success" >> exitSuccess
+        True ->
+          deploy c (DeployCLI {deployHosts = hs, pubfile = allPubfile r, envir = allEnvironment r}) >>= \case
+            False -> putText "deployment couldn't succeed.. " >> tryDeploy hs
+            True -> putText "deployment success" >> exitSuccess
 
 prompt :: Text -> IO Text
 prompt xs = putText (xs <> " ") >> getLine
 
 hostsAndPrint :: CommonOpts -> UID -> IO ()
-hostsAndPrint c i = hosts c i >>= \case
-  Nothing -> die "Query failure"
-  Just (Hosts hosts) -> putText $ "job has hosts " <> show hosts
+hostsAndPrint c i =
+  hosts c i >>= \case
+    Nothing -> die "Query failure"
+    Just (Hosts hosts) -> putText $ "job has hosts " <> show hosts
 
 reserveAndPrint :: CommonOpts -> ReservationCLI -> IO ()
-reserveAndPrint c r = reserve c r >>= \case
-  Nothing -> die "Reservation failure"
-  Just (UID id) -> putText $ "Reserved job ID " <> show id
+reserveAndPrint c r =
+  reserve c r >>= \case
+    Nothing -> die "Reservation failure"
+    Just (UID id) -> putText $ "Reserved job ID " <> show id
 
 deployAndPrint :: CommonOpts -> DeployCLI -> IO ()
-deployAndPrint c d = deploy c d >>= \case
-  True -> putText $ "deployed CLI deployment request" <> show d
-  _ -> die "Deployment request failure"
+deployAndPrint c d =
+  deploy c d >>= \case
+    True -> putText $ "deployed CLI deployment request" <> show d
+    _ -> die "Deployment request failure"
 
 parseOpts :: OA.Parser (IO ())
 parseOpts =
@@ -233,7 +237,7 @@ parserPubfile :: OA.Parser Pubfile
 parserPubfile = Pubfile <$> OA.strOption ((metavar "PUBKEY") <> long "pubfile" <> short 'k')
 
 parserEnvironment :: OA.Parser Environment
-parserEnvironment = Environment <$> OA.strOption ((metavar "ENVIRONMENT") <> value "wheezy-x64-base" <> long "environment" <> short 'e')
+parserEnvironment = Environment <$> OA.strOption ((metavar "ENVIRONMENT") <> value "http://public.nancy.grid5000.fr/~orichard/nixos-19.09.yaml" <> long "environment" <> short 'e')
 
 parserDeployCLI :: OA.Parser DeployCLI
 parserDeployCLI = DeployCLI <$> parserHosts <*> parserPubfile <*> parserEnvironment
@@ -244,9 +248,9 @@ parserReservationCLI =
     <$> ( NodeCount
             <$> OA.option auto (metavar "nodes" <> long "nodes" <> value 1)
         )
-      <*> ( Walltime
-              <$> OA.strOption (metavar "walltime" <> long "walltime" <> value "00:10")
-          )
+    <*> ( Walltime
+            <$> OA.strOption (metavar "walltime" <> long "walltime" <> value "01:00")
+        )
 
 parserCommon :: OA.Parser CommonOpts
 parserCommon =
