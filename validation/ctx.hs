a3fd48fac0d558cc7bd9307d7378b4f089170e84
Valentin Reis <vreis@anl.gov>
2020-03-09 23:27:45 -0500

    validation update

--- validation/ctx.hs
+++ validation/ctx.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Control.Lens
 import Control.Monad.Primitive
@@ -8,14 +10,18 @@ import Data.Sequence
 import H.Prelude as R
 import HBandit.Class
 import HBandit.Exp4R
+import HBandit.Types
 import Protolude
+import Refined.Unsafe
 import System.Random
+
+type ZO = ZeroOne Double
 
 rrequire :: (MonadR m, Literal lib a) => lib -> m ()
 rrequire p = void [r| suppressMessages(require(p_hs,character.only=TRUE)) |]
 
 rpackages :: [Text]
-rpackages = ["ggplot2", "plotly", "latex2exp"]
+rpackages = ["ggplot2", "plyr", "plotly", "latex2exp"]
 
 data OutputType = Plotly | Pdf | Png
   deriving (Read, Show, Enum, Bounded)
@@ -38,38 +44,57 @@ saveTsPlot ggplot path = \case
 
 data GameState
   = GameState
-      { history :: Seq Double,
+      { history :: Seq Int,
         bandit :: Exp4R () Int
       }
   deriving (Generic)
 
 onePass ::
-  (MonadState GameState m, MonadIO m) =>
-  [(Double, Double, Double, Double)] ->
+  ( MonadState GameState m,
+    MonadIO m,
+    (Functor (Zoomed m' (Int, StdGen))),
+    (Zoom m' m (Exp4R () Int) GameState)
+  ) =>
+  [(ZO, ZO, ZO, ZO)] ->
   m ()
 onePass dataset =
-  for_ dataset $ \(x, y) -> do
+  for_ dataset $ \(c1, c2, r1, r2) -> do
+    oldA <- use (field @"bandit" . field @"lastAction")
+    let (c, r) = if oldA == 1 then (c1, r1) else (c2, r2)
     g <- liftIO getStdGen
-    (a, g') <- zoom (field @"bandit") $ do
-      stepCtx g undefined ()
+    (a, g') <- zoom (field @"bandit") $ stepCtx g (Feedback c r) ()
     liftIO $ setStdGen g'
-    field @"history" %= (Data.Sequence.|> undefined)
+    field @"history" %= (Data.Sequence.|> a)
 
 plot1pass ::
   (MonadR m, MonadIO m) =>
   Double ->
-  [(Double, Double, Double, Double)] ->
+  [Double] ->
+  [Double] ->
+  [Double] ->
+  [Double] ->
   m (SomeSEXP (PrimState m))
-plot1pass comparator fit dataset = do
+plot1pass comparator one_cost two_cost one_risk two_risk = do
   g <- liftIO getStdGen
-  (b, a, g') <- initCtx g undefined
-  let (GameState (toList -> history) _) =
-        execState (onePass fit dataset) $ GameState
-          { history = Data.Sequence.Empty,
-            bandit = b
-          }
+  let (b, a, g') = initCtx g Exp4RCfg
+        { expertsCfg = expertsC,
+          constraintCfg = unsafeRefine 0.4,
+          horizonCfg = unsafeRefine 100,
+          as = 1 :| [2]
+        }
+  (GameState ((fmap fromIntegral) . toList -> history :: [Double]) _) <- execStateT (onePass dataset) $
+    GameState
+      { history = Data.Sequence.empty,
+        bandit = b
+      }
   [r|
-    d <- data.frame(seq(1, length(ys_hs), 1), ys_hs, history_hs)
+    d <- data.frame(seq(1, length(history_hs), 1),
+                    history_hs,
+                    one_cost_hs,
+                    two_cost_hs,
+                    one_risk_hs,
+                    two_risk_hs)
+    summary(d)
     names(d)=c("t","label","history")
     d$c = cumsum((d$history - d$label)^2)
     print(head(d))
@@ -82,38 +107,32 @@ plot1pass comparator fit dataset = do
       ylab(TeX("\\frac{1}{t} \\sum_{i=1}^{i=t} c^i (x^i)"))
   |]
   where
-    ys = snd <$> dataset
-
-preamble :: (MonadR m) => m (SomeSEXP (PrimState m))
-preamble =
-  [r|
-    set.seed(1)
-    k = 3
-    n = 100
-    mus = pmax(0,pmin(1,rnorm(k, mean=0.5, sd=1)))
-    make_arm <- function(mu) pmax(0,pmin(1,rnorm(n, mean=mu, sd=0.1)))
-    d = as.data.frame(t(ldply(mus,make_arm)))
-    names(d) = as.character(1:k)
-    summary(d)
-|]
+    expertsC :: NonEmpty (() -> NonEmpty (ZeroOne Double, Int))
+    expertsC = expert1 :| [expert2]
+    expert1 () = [(HBandit.Types.one, 1 :: Int), (HBandit.Types.zero, 2 :: Int)]
+    expert2 () = [(HBandit.Types.one, 2 :: Int), (HBandit.Types.zero, 1 :: Int)]
+    p = ZipList . fmap unsafeRefine
+    (ZipList dataset) =
+      (\a b c d -> (a, b, c, d) :: (ZO, ZO, ZO, ZO))
+        <$> p one_cost
+        <*> p two_cost
+        <*> p one_risk
+        <*> p two_risk
 
 experiment ::
   (MonadR m) =>
+  Double ->
   m (SomeSEXP (PrimState m))
-experiment = do
-  one_cost <- [r| d$"1"|]
-  two_cost <- [r| d$"2" |]
-  one_risk <- [r| d$"1"|]
-  two_risk <- [r| d$"2" |]
-  let (ZipList dataset) =
-        (\a b c d -> (a, b, c, d) :: (Double, Double, Double, Double))
-          <$> p one_cost
-          <*> p two_cost
-          <*> p one_risk
-          <*> p two_risk
-  plot1pass 0.4 dataset
+experiment n = do
+  one_cost <- gen01TS <&> fromSomeSEXP
+  two_cost <- gen01TS <&> fromSomeSEXP
+  one_risk <- gen01TS <&> fromSomeSEXP
+  two_risk <- gen01TS <&> fromSomeSEXP
+  plot1pass 0.4 one_cost two_cost one_risk two_risk
   where
-    p = ZipList . fromSomeSEXP
+    gen01TS =
+      [r| mu = pmax(0,pmin(1,rnorm(1, mean=0.5, sd=1)))
+          pmax(0,pmin(1,rnorm(n_hs, mean=mu, sd=0.1))) |]
 
 main :: IO ()
 main =
@@ -122,5 +141,4 @@ main =
     $ do
       for_ rpackages rrequire
       [r| theme_set(theme_bw()) |]
-      _ <- preamble
-      (experiment >>= \x -> void $ saveTsPlot x "plotBandit" Png)
+      (experiment 1000 >>= \x -> void $ saveTsPlot x "plotBandit" Png)
