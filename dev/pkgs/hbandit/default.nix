5aa41727db2c71de720daff6527ea778c51bc5ba
Valentin Reis <vreis@anl.gov>
2020-05-15 11:38:50 -0500

    switch to hbandit release v0.1

--- dev/pkgs/hbandit/default.nix
+++ dev/pkgs/hbandit/default.nix
@@ -3,8 +3,8 @@
 mkDerivation {
   src = fetchurl {
     url =
-      "https://xgitlab.cels.anl.gov/argo/hbandit/-/archive/master/hbandit-master.tar.gz";
-    sha256 = "05060rg02nqcmvgwb3ywk4psc8xqqma4hwqd2d2bb3clmr0izdmf";
+      "https://xgitlab.cels.anl.gov/argo/hbandit/-/archive/v0.1/hbandit-v0.1.tar.gz";
+    sha256 = "02psclmlvz94axwyx17pzscb8c5s92d2q2lcnrrbx56kpgy3gwkw";
   };
   pname = "hbandit";
   version = "1.0.0";
